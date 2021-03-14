5bef08288166a03964abcc0faa4ba60b34442b4d
Patrick de Mars <pwdemars@gmail.com>
2021-03-14 16:32:50 +0000

    updated hpc_scripts

--- hpc_scripts/test_tree_search.sh
+++ hpc_scripts/test_tree_search.sh
@@ -8,7 +8,7 @@
 
 save_dir=$1
 params_filename=$2
-arma_params_filename=$3
+env_params_filename=$3
 policy_filename=$4
 horizon=$5
 branching_threshold=$6
@@ -26,5 +26,5 @@ module load python3/3.7
 
 cd $TMPDIR
 
-python $HOME/ts4uc/ts4uc/tree_search/day_ahead.py --save_dir $save_dir --policy_params_fn $params_filename --arma_params_fn $arma_params_filename --policy_filename $policy_filename --test_data $test_data --branching_threshold $branching_threshold --horizon $horizon --num_scenarios 100 --tree_search_func_name $tree_search_func_name
+python $HOME/ts4uc/ts4uc/tree_search/day_ahead.py --save_dir $save_dir --policy_params_fn $params_filename --env_params_fn $env_params_filename --policy_filename $policy_filename --test_data $test_data --branching_threshold $branching_threshold --horizon $horizon --num_scenarios 100 --tree_search_func_name $tree_search_func_name
 
