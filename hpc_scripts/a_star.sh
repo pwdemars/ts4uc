0465cf9a38564449f70e6f9ab2c23067a45f95af
Patrick de Mars <pwdemars@gmail.com>
2021-03-17 16:52:46 +0000

    changed names

--- hpc_scripts/a_star.sh
+++ hpc_scripts/a_star.sh
@@ -11,7 +11,7 @@ qsub -l h_rt=2:00:00 test_tree_search.sh $HOME/Scratch/results/${date}/guided_as
 qsub -l h_rt=2:00:00 test_tree_search.sh $HOME/Scratch/results/${date}/guided_astar/g30/feb4_g30_d30_v3_h4_p05 $HOME/AISO_HPC/feb4/g30/g30_d30_v3.json $HOME/AISO_HPC/mar14/30_env_params.json $HOME/Scratch/results/feb4_g30_d30_v3/ac_final.pt 4 0.05 a_star $HOME/AISO_HPC/AISO/input_g30_d30.txt
 
 # unguided a* search, g={5,10,20,30}, H=2
-qsub -l h_rt=1:00:00 test_tree_search.sh $HOME/Scratch/results/${date}/unguided_astar/g5/feb4_g5_d30_v1_h4_p05 none $HOME/AISO_HPC/mar14/5_env_params.json none 2 -1 a_star $HOME/AISO_HPC/AISO/input_g5_d30.txt
+qsub -l h_rt=1:00:00 test_tree_search.sh $HOME/Scratch/results/${date}/unguided_astar/g5/h2 none $HOME/AISO_HPC/mar14/5_env_params.json none 2 -1 a_star $HOME/AISO_HPC/AISO/input_g5_d30.txt
 
 # guided real-time a* search, g={5,10,20,30}, H=4, rho=0.05
 # use policies *_v1 for g5, g10 and g20, use *_v3 for g30. 
@@ -21,4 +21,4 @@ qsub -l h_rt=2:00:00 test_tree_search.sh $HOME/Scratch/results/${date}/guided_rt
 qsub -l h_rt=2:00:00 test_tree_search.sh $HOME/Scratch/results/${date}/guided_rtastar/g30/feb4_g30_d30_v3_h4_p05 $HOME/AISO_HPC/feb4/g30/g30_d30_v3.json $HOME/AISO_HPC/mar14/30_env_params.json $HOME/Scratch/results/feb4_g30_d30_v3/ac_final.pt 4 0.05 rta_star $HOME/AISO_HPC/AISO/input_g30_d30.txt
 
 # unguided real-time a* search, g={5,10,20,30}, H=2
-qsub -l h_rt=1:00:00 test_tree_search.sh $HOME/Scratch/results/${date}/unguided_rtastar/g5/feb4_g5_d30_v1_h4_p05 none $HOME/AISO_HPC/mar14/5_env_params.json none 2 -1 rta_star $HOME/AISO_HPC/AISO/input_g5_d30.txt
+qsub -l h_rt=1:00:00 test_tree_search.sh $HOME/Scratch/results/${date}/unguided_rtastar/g5/h2 none $HOME/AISO_HPC/mar14/5_env_params.json none 2 -1 rta_star $HOME/AISO_HPC/AISO/input_g5_d30.txt
