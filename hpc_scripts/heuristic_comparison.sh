eba7254df8a1ad14f8d0464a8d76449e2a368b41
Patrick de Mars <pwdemars@gmail.com>
2021-03-22 18:03:37 +0000

    fixed multi-line bash command

--- hpc_scripts/heuristic_comparison.sh
+++ hpc_scripts/heuristic_comparison.sh
@@ -3,6 +3,23 @@
 # set today's date to use as save directory
 date=$(date +"%y-%m-%d")
 
-for h in "check_lost_load" "priority_list" "pl_plus_ll";
-do qsub -l h_rt=2:00:00 test_tree_search.sh $HOME/Scratch/results/${date}/guided_astar/g5/feb4_g5_d30_v1_h4_p05_${h} $HOME/AISO_HPC/feb4/g5/g5_d30_v1.json $HOME/AISO_HPC/mar14/5_env_params.json $HOME/Scratch/results/feb4_g5_d30_v1/ac_final.pt 4 0.05 a_star $HOME/AISO_HPC/AISO/input_g5_d30.txt $h ; 
-done
+# Different for 30 gens! 
+for g in 5 10 20;
+	do for tree_search_func_name in a_star rta_star;
+		do for H in 2 4 6; 
+			do for rho in 0.05;
+				do for heuristic_method in "check_lost_load" "priority_list" "pl_plus_ll";
+					do qsub -l h_rt=12:00:00 test_tree_search.sh $HOME/Scratch/results/${date}/guided_${tree_search_func_name}/g5/feb4_g5_d30_v1_h${h}_p${rho}_${heuristic_method} \
+																 $HOME/AISO_HPC/feb4/g${g}/g${g}_d30_v1.json \
+																 $HOME/AISO_HPC/mar14/${g}_env_params.json \
+																 $HOME/Scratch/results/feb4_g${g}_d30_v1/ac_final.pt \
+																 ${H} \
+																 ${rho} \
+																 ${tree_search_func_name} \
+																 $HOME/AISO_HPC/AISO/input_g${g}_d30.txt \
+																 $heuristic_method ; 
+				done ;
+			done ;
+		done ;
+	done ;
+done ;
