03130738d8a475243828cd54474f19e963b16f97
Patrick de Mars <pwdemars@gmail.com>
2021-03-26 10:40:18 +0000

    added advanced priority list to experiment 201

--- hpc_scripts/201-heuristic_comparison.sh
+++ hpc_scripts/201-heuristic_comparison.sh
@@ -8,7 +8,7 @@ for g in 5 10 20 30;
 	do for tree_search_func_name in a_star rta_star;
 		do for H in 2 4 6; 
 			do for rho in 05;
-				do for heuristic_method in "check_lost_load" "priority_list" "pl_plus_ll";
+				do for heuristic_method in "check_lost_load" "priority_list" "pl_plus_ll" "advanced_priority_list";
 					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided_${tree_search_func_name}/g${g}/feb4_g${g}_d30_v1_h${H}_p${rho}_${heuristic_method} \
 																 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 																 $HOME/AISO_HPC/mar14/${g}_env_params.json \
