c9b0ab724a65658595527f07a50efedfa1840abc
Patrick de Mars <pwdemars@gmail.com>
2021-03-30 18:32:50 +0100

    increasing time budget (factor of 2)

--- hpc_scripts/202-anytime_ida_star.sh
+++ hpc_scripts/202-anytime_ida_star.sh
@@ -9,8 +9,8 @@ tree_search_func_name="a_star"
 for g in 10 20 30; 
   do for t in 1 2 5 10 30 60;
     do for heuristic_method in "check_lost_load" "priority_list" "advanced_priority_list";
-      do let secs=$t*52 && \
-         time=$(date -d@36 -u +%H:%M:%S) && \
+      do let secs=$t*96 && \
+         time=$(date -d@$secs -u +%H:%M:%S) && \
          qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}/guided_${tree_search_func_name}/g${g}/feb4_g${g}_d30_v1_t${t}_p${rho}_${heuristic_method} \
 														 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 														 $HOME/AISO_HPC/mar14/${g}_env_params.json \
