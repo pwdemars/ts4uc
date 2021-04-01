5a4d9b77b40ef89c4ade66a692839173e90f0035
Patrick de Mars <pwdemars@gmail.com>
2021-04-01 17:50:28 +0100

    updated experiments: removed pl_plus_ll heuristic

--- hpc_scripts/201-heuristic_comparison.sh
+++ hpc_scripts/201-heuristic_comparison.sh
@@ -4,33 +4,12 @@
 date=$(date +"%y-%m-%d")
 
 # Different for 30 gens! 
-for g in 5 10 20;
+for g in 5 10 20 30;
 	do for tree_search_func_name in a_star rta_star;
 		do for H in 2 4 6; 
 			do for rho in 05;
-				do for heuristic_method in "check_lost_load" "priority_list" "pl_plus_ll" "advanced_priority_list";
-					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided_${tree_search_func_name}/g${g}/feb4_g${g}_d30_v1_h${H}_p${rho}_${heuristic_method} \
-																 $HOME/AISO_HPC/best_policies/g${g}/params.json \
-																 $HOME/AISO_HPC/mar14/${g}_env_params.json \
-																 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
-																 ${H} \
-																 0.${rho} \
-																 ${tree_search_func_name} \
-																 $HOME/AISO_HPC/AISO/input_g${g}_d30.txt \
-																 $heuristic_method ; 
-				done ;
-			done ;
-		done ;
-	done ;
-done ;
-
-# 30 gens uses different policy: feb4_g30_d30_v3 (not v1) 
-for g in 30;
-	do for tree_search_func_name in a_star rta_star;
-		do for H in 2 4 6; 
-			do for rho in 05;
-				do for heuristic_method in "check_lost_load" "priority_list" "pl_plus_ll" "advanced_priority_list";
-					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided_${tree_search_func_name}/g${g}/feb4_g${g}_d30_v3_h${H}_p${rho}_${heuristic_method} \
+				do for heuristic_method in "check_lost_load" "priority_list" "advanced_priority_list" "none";
+					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided_${tree_search_func_name}/g${g}/feb4_g${g}_h${H}_p${rho}_${heuristic_method} \
 																 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 																 $HOME/AISO_HPC/mar14/${g}_env_params.json \
 																 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
--- hpc_scripts/202-anytime_ida_star.sh
+++ hpc_scripts/202-anytime_ida_star.sh
@@ -8,10 +8,10 @@ rho=05
 tree_search_func_name="a_star"
 for g in 10 20 30; 
   do for t in 1 2 5 10 30 60;
-    do for heuristic_method in "check_lost_load" "priority_list" "advanced_priority_list";
+    do for heuristic_method in "check_lost_load" "priority_list" "advanced_priority_list" "none";
       do let secs=$t*96 && \
          time=$(date -d@$secs -u +%H:%M:%S) && \
-         qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}/guided_${tree_search_func_name}/g${g}/feb4_g${g}_d30_v1_t${t}_p${rho}_${heuristic_method} \
+         qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}/guided_${tree_search_func_name}/g${g}/feb4_g${g}_t${t}_p${rho}_${heuristic_method} \
 														 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 														 $HOME/AISO_HPC/mar14/${g}_env_params.json \
 														 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
