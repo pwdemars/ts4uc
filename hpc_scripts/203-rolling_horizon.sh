4b76bf9ac5e1fd55f96c70a8cda40b78c377f0ff
Patrick de Mars <pwdemars@gmail.com>
2021-04-07 13:41:16 +0100

    removed UCS from experiment 203

--- hpc_scripts/203-rolling_horizon.sh
+++ hpc_scripts/203-rolling_horizon.sh
@@ -8,7 +8,7 @@ rho=05
 tree_search_func_name="ida_star"
 for g in 10 20 30; 
   do for t in 1 2 5 10 30 60;
-    do for heuristic_method in "none" "advanced_priority_list";
+    do for heuristic_method in "advanced_priority_list";
       do let secs=$t*48*2 && \
          time=$(date -d@$secs -u +%H:%M:%S) && \
          qsub -l h_rt=$time submit_rolling_tree_search.sh $HOME/Scratch/results/${date}/rolling_guided_${tree_search_func_name}/g${g}/g${g}_t${t}_p${rho}_${heuristic_method} \
