2c53f9ed904e56492f8216e46098252e5157f5fc
Patrick de Mars <pwdemars@gmail.com>
2021-04-08 19:11:47 +0100

    Renamed experiment save_dirs

--- hpc_scripts/103-guided_vs_unguided.sh
+++ hpc_scripts/103-guided_vs_unguided.sh
@@ -5,10 +5,10 @@ date=$(date +"%y-%m-%d")
 
 # guided search, g={5..10}, H=2, rho=0.05
 for g in {5..10};
-do qsub -l h_rt=0:30:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided/g${g}/feb4_g${g}_d30_v1_h2_p05 $HOME/AISO_HPC/feb4/g${g}/g${g}_d30_v1.json $HOME/AISO_HPC/mar14/${g}_env_params.json $HOME/Scratch/results/feb4_g${g}_d30_v1/ac_final.pt 2 0.05 uniform_cost_search $HOME/AISO_HPC/AISO/input_g${g}_d30.txt;
+do qsub -l h_rt=0:30:00 submit_tree_search.sh $HOME/Scratch/results/${date}_103/guided/g${g}/g${g}_h2_p05 $HOME/AISO_HPC/feb4/g${g}/g${g}_d30_v1.json $HOME/ts4uc/data/day_ahead/${g}gen/30mins/env_params.json $HOME/Scratch/results/feb4_g${g}_d30_v1/ac_final.pt 2 0.05 uniform_cost_search $HOME/AISO_HPC/AISO/input_g${g}_d30.txt;
 done
 
 # unguided search, g={5..10}, H=2
 for g in {5..10};
-do qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/unguided/g${g}/h2 none $HOME/AISO_HPC/mar14/${g}_env_params.json none 2 -1 uniform_cost_search $HOME/AISO_HPC/AISO/input_g${g}_d30.txt;
+do qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_103/unguided/g${g}_h2 none $HOME/ts4uc/data/day_ahead/${g}gen/30mins/env_params.json none 2 -1 uniform_cost_search $HOME/AISO_HPC/AISO/input_g${g}_d30.txt;
 done
