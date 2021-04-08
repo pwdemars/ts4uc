80e9a4619858a6ef9359ea29f49473a60c4dabc7
Patrick de Mars <pwdemars@gmail.com>
2021-04-08 19:27:51 +0100

    Renaming save_dirs and file locations

--- hpc_scripts/104-guided_parameter_comparison.sh
+++ hpc_scripts/104-guided_parameter_comparison.sh
@@ -6,7 +6,7 @@ date=$(date +"%y-%m-%d")
 # guided search, g=5; H={1, 2, 4, 6, 8, 12, 16, 20, 24}; rho={0.01, 0.02, 0.05, 0.1, 0.25, 0.33}
 for H in 1 2 4;
 do for rho in 01 05;
-do qsub -l h_rt=6:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
+do qsub -l h_rt=6:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
 	$HOME/AISO_HPC/feb4/g5/g5_d30_v1.json \
 	$HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json \
 	$HOME/Scratch/results/feb4_g5_d30_v1/ac_final.pt \
@@ -19,7 +19,7 @@ done
 
 for H in 1 2 4;
 do for rho in 1 25 33;
-do qsub -l h_rt=1:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
+do qsub -l h_rt=1:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
 	$HOME/AISO_HPC/feb4/g5/g5_d30_v1.json \
 	$HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json \
 	$HOME/Scratch/results/feb4_g5_d30_v1/ac_final.pt \
@@ -32,7 +32,7 @@ done
 
 for H in 6 8;
 do for rho in 01 05 1 25 33;
-do qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
+do qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
 	$HOME/AISO_HPC/feb4/g5/g5_d30_v1.json \
 	$HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json \
 	$HOME/Scratch/results/feb4_g5_d30_v1/ac_final.pt \
@@ -45,7 +45,7 @@ done
 
 for H in 12 16 20 24;
 do for rho in 05 1 25 33;
-do qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
+do qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
 	$HOME/AISO_HPC/feb4/g5/g5_d30_v1.json \
 	$HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json \
 	$HOME/Scratch/results/feb4_g5_d30_v1/ac_final.pt \
