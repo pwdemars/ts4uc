8256a9f0d1ef888023ef1e9ea109463194672c34
Patrick de Mars <pwdemars@gmail.com>
2021-04-08 17:44:02 +0100

    Corrected save_dir name

--- hpc_scripts/102-observation_processor_comparison.sh
+++ hpc_scripts/102-observation_processor_comparison.sh
@@ -3,4 +3,4 @@
 date=$(date +"%y-%m-%d")
 
 qsub -pe smp 8 -l h_rt=6:00:00 ./submit_train.sh ${date}_102_v1 $HOME/AISO_HPC/mar23/mar23_g5_v1.json $HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json 8 25000
-qsub -pe smp 8 -l h_rt=6:00:00 ./submit_train.sh ${date}_103_v2 $HOME/AISO_HPC/mar23/mar23_g5_v2.json $HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json 8 25000
+qsub -pe smp 8 -l h_rt=6:00:00 ./submit_train.sh ${date}_102_v2 $HOME/AISO_HPC/mar23/mar23_g5_v2.json $HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json 8 25000
