f399446b6700b9ac5fe88ecb1e2331d3a6dc3a0d
Patrick de Mars <pwdemars@gmail.com>
2021-04-09 14:40:19 +0100

    Point to parameter files within ts4uc

--- hpc_scripts/101-train_policies.sh
+++ hpc_scripts/101-train_policies.sh
@@ -8,33 +8,33 @@ g=5
 workers=8
 epochs=25000
 hrs=4
-qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh ${date}_101/g${g} $HOME/AISO_HPC/best_policies/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $workers $epochs
+qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh ${date}_101/g${g} $HOME/ts4uc/data/policy_params/exp101/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $workers $epochs
 
 # 10 gen: 8 workers, 100,000 epochs
 g=10
 workers=8
 epochs=100000
 hrs=12
-qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh ${date}_101/g${g} $HOME/AISO_HPC/best_policies/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $workers $epochs
+qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh ${date}_101/g${g} $HOME/ts4uc/data/policy_params/exp101/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $workers $epochs
 
 # 20 gen: 8 workers, 100,000 epochs
 g=20
 workers=8
 epochs=100000
 hrs=12
-qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh ${date}_101/g${g} $HOME/AISO_HPC/best_policies/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $workers $epochs
+qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh ${date}_101/g${g} $HOME/ts4uc/data/policy_params/exp101/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $workers $epochs
 
 # 30 gen: 8 workers, 200,000 epochs
 g=30
 workers=8
 epochs=200000
 hrs=24
-qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh ${date}_101/g${g} $HOME/AISO_HPC/best_policies/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $workers $epochs
+qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh ${date}_101/g${g} $HOME/ts4uc/data/policy_params/exp101/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $workers $epochs
 
 # 6, 7, 8, 9 gens
 workers=8
 epochs=25000
 hrs=4
 for g in 6 7 8 9;
-do qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh ${date}_101/g${g} $HOME/AISO_HPC/best_policies/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $workers $epochs ;
+do qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh ${date}_101/g${g} $HOME/ts4uc/data/policy_params/exp101/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $workers $epochs ;
 done
--- hpc_scripts/102-observation_processor_comparison.sh
+++ hpc_scripts/102-observation_processor_comparison.sh
@@ -2,5 +2,5 @@
 
 date=$(date +"%y-%m-%d")
 
-qsub -pe smp 8 -l h_rt=6:00:00 ./submit_train.sh ${date}_102/v1 $HOME/AISO_HPC/mar23/mar23_g5_v1.json $HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json 8 25000
-qsub -pe smp 8 -l h_rt=6:00:00 ./submit_train.sh ${date}_102/v2 $HOME/AISO_HPC/mar23/mar23_g5_v2.json $HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json 8 25000
+qsub -pe smp 8 -l h_rt=6:00:00 ./submit_train.sh ${date}_102/v1 $HOME/ts4uc/data/policy_params/exp102/params_DA.json $HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json 8 25000
+qsub -pe smp 8 -l h_rt=6:00:00 ./submit_train.sh ${date}_102/v2 $HOME/ts4uc/data/policy_params/exp102/params_LH.json $HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json 8 25000
