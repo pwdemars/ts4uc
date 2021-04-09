956916d766619342e11fda1d33f7a79fd393b128
Patrick de Mars <pwdemars@gmail.com>
2021-04-09 09:49:10 +0100

    Point to ts4uc/data/.../env_params.json

--- hpc_scripts/201-heuristic_comparison.sh
+++ hpc_scripts/201-heuristic_comparison.sh
@@ -11,7 +11,7 @@ for g in 10 20 30;
 				do for heuristic_method in "check_lost_load" "priority_list" "advanced_priority_list" "none";
 					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided_${tree_search_func_name}/g${g}/feb4_g${g}_h${H}_p${rho}_${heuristic_method} \
 																 $HOME/AISO_HPC/best_policies/g${g}/params.json \
-																 $HOME/AISO_HPC/mar14/${g}_env_params.json \
+																 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
 																 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
 																 ${H} \
 																 0.${rho} \
