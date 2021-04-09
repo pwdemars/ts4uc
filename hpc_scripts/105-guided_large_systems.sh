d9d17156a3c5485b894c8d3b9d1ed142a3d37bbf
Patrick de Mars <pwdemars@gmail.com>
2021-04-09 09:07:49 +0100

    Pointing to best_policies directory

--- hpc_scripts/105-guided_large_systems.sh
+++ hpc_scripts/105-guided_large_systems.sh
@@ -4,7 +4,6 @@
 date=$(date +"%y-%m-%d")
 
 # guided search, g={10,20,30}, H=4, rho=0.05
-# use policies *_v1 for g10 and g20, use *_v3 for g30. 
-qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided/g10/feb4_g10_d30_v1_h4_p05 $HOME/AISO_HPC/feb4/g10/g10_d30_v1.json $HOME/AISO_HPC/mar14/10_env_params.json $HOME/Scratch/results/feb4_g10_d30_v1/ac_final.pt 4 0.05 uniform_cost_search $HOME/AISO_HPC/AISO/input_g10_d30.txt
-qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided/g20/feb4_g20_d30_v1_h4_p05 $HOME/AISO_HPC/feb4/g20/g20_d30_v1.json $HOME/AISO_HPC/mar14/20_env_params.json $HOME/Scratch/results/feb4_g20_d30_v1/ac_final.pt 4 0.05 uniform_cost_search $HOME/AISO_HPC/AISO/input_g20_d30.txt
-qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided/g30/feb4_g30_d30_v3_h4_p05 $HOME/AISO_HPC/feb4/g30/g30_d30_v3.json $HOME/AISO_HPC/mar14/30_env_params.json $HOME/Scratch/results/feb4_g30_d30_v3/ac_final.pt 4 0.05 uniform_cost_search $HOME/AISO_HPC/AISO/input_g30_d30.txt
+for g in 10 20 30;
+do qsub -h h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/${date}_105/g${g} $HOME/AISO_HPC/best_policies/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt 4 0.05 uniform_cost_search $HOME/AISO_HPC/AISO/input_g${g}_d30.txt ;
+done ;
