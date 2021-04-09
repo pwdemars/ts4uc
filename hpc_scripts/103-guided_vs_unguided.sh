b7a30119a2b2c4201b2d21cc68a042cf08df6c56
Patrick de Mars <pwdemars@gmail.com>
2021-04-09 12:06:30 +0100

    Again pointing to best_policies directory: leave no trace of feb4

--- hpc_scripts/103-guided_vs_unguided.sh
+++ hpc_scripts/103-guided_vs_unguided.sh
@@ -5,7 +5,7 @@ date=$(date +"%y-%m-%d")
 
 # guided search, g={5..10}, H=2, rho=0.05
 for g in {5..10};
-do qsub -l h_rt=0:30:00 submit_tree_search.sh $HOME/Scratch/results/${date}_103/guided/g${g}/g${g}_h2_p05 $HOME/AISO_HPC/feb4/g${g}/g${g}_d30_v1.json $HOME/ts4uc/data/day_ahead/${g}gen/30mins/env_params.json $HOME/Scratch/results/feb4_g${g}_d30_v1/ac_final.pt 2 0.05 uniform_cost_search $HOME/AISO_HPC/AISO/input_g${g}_d30.txt;
+do qsub -l h_rt=0:30:00 submit_tree_search.sh $HOME/Scratch/results/${date}_103/guided/g${g}/g${g}_h2_p05 $HOME/AISO_HPC/best_policies/g${g}/params.json $HOME/ts4uc/data/day_ahead/${g}gen/30mins/env_params.json $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt 2 0.05 uniform_cost_search $HOME/AISO_HPC/AISO/input_g${g}_d30.txt;
 done
 
 # unguided search, g={5..10}, H=2
--- hpc_scripts/104-guided_parameter_comparison.sh
+++ hpc_scripts/104-guided_parameter_comparison.sh
@@ -6,7 +6,7 @@ date=$(date +"%y-%m-%d")
 # guided search, g=5; H={1, 2, 4, 6, 8, 12, 16, 20, 24}; rho={0.01, 0.02, 0.05, 0.1, 0.25, 0.33}
 for H in 1 2 4;
 do for rho in 01 05;
-do qsub -l h_rt=6:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
+do qsub -l h_rt=6:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/h${H}_p${rho} \
 	$HOME/AISO_HPC/best_policies/g5/params.json \
 	$HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json \
 	$HOME/AISO_HPC/best_policies/g5/ac_final.pt \
@@ -19,7 +19,7 @@ done
 
 for H in 1 2 4;
 do for rho in 1 25 33;
-do qsub -l h_rt=1:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
+do qsub -l h_rt=1:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/h${H}_p${rho} \
 	$HOME/AISO_HPC/best_policies/g5/params.json \
 	$HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json \
 	$HOME/AISO_HPC/best_policies/g5/ac_final.pt \
@@ -32,7 +32,7 @@ done
 
 for H in 6 8;
 do for rho in 01 05 1 25 33;
-do qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
+do qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/h${H}_p${rho} \
 	$HOME/AISO_HPC/best_policies/g5/params.json \
 	$HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json \
 	$HOME/AISO_HPC/best_policies/g5/ac_final.pt \
@@ -45,7 +45,7 @@ done
 
 for H in 12 16 20 24;
 do for rho in 05 1 25 33;
-do qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/feb4_g5_d30_v1_h${H}_p${rho} \
+do qsub -l h_rt=24:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_104/guided/g5/h${H}_p${rho} \
 	$HOME/AISO_HPC/best_policies/g5/params.json \
 	$HOME/ts4uc/data/day_ahead/5gen/30min/env_params.json \
 	$HOME/AISO_HPC/best_policies/g5/ac_final.pt \
