41ae6d31b3b295f6733439baed827ae770a2e39e
Patrick de Mars <pwdemars@gmail.com>
2021-04-11 13:44:56 +0100

    Renamed save_dirs

--- hpc_scripts/201-heuristic_comparison.sh
+++ hpc_scripts/201-heuristic_comparison.sh
@@ -9,7 +9,7 @@ for g in 10 20 30;
 		do for H in 2 4 6; 
 			do for rho in 05;
 				do for heuristic_method in "check_lost_load" "priority_list" "advanced_priority_list" "none";
-					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}/guided_${tree_search_func_name}/g${g}/feb4_g${g}_h${H}_p${rho}_${heuristic_method} \
+					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_201/guided_${tree_search_func_name}/g${g}/feb4_g${g}_h${H}_p${rho}_${heuristic_method} \
 																 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 																 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
 																 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
