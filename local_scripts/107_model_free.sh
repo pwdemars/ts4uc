c59ad7359bc61bd08c8ef34a7581319ab045cc44
Patrick de Mars <pwdemars@gmail.com>
2021-04-11 11:49:37 +0100

    Renamed test_model_free --> solve_model_free

--- local_scripts/107_model_free.sh
+++ local_scripts/107_model_free.sh
@@ -4,5 +4,5 @@
 date=$(date +"%y-%m-%d")
 
 for g in 10 20 30 ;
-do python $HOME/ts4uc/ts4uc/agents/test_model_free.py -s $HOME/ts4uc/results/${date}_107/g${g}_mf -e $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json -t $HOME/ts4uc/data/day_ahead/${g}gen/30min -pf $HOME/thesis/results/chapter1/new_results/policies/exp101/g${g}/ac_final.pt -pp $HOME/thesis/results/chapter1/new_results/policies/exp101/g${g}/params.json --num_samples 1000 ;
+do python $HOME/ts4uc/ts4uc/agents/solve_model_free.py -s $HOME/ts4uc/results/${date}_107/g${g}_mf -e $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json -t $HOME/ts4uc/data/day_ahead/${g}gen/30min -pf $HOME/thesis/results/chapter1/new_results/policies/exp101/g${g}/ac_final.pt -pp $HOME/thesis/results/chapter1/new_results/policies/exp101/g${g}/params.json --num_samples 1000 ;
    done 
