e19a8476a46a81ce24ba7a0d76ecafa128bd667a
Patrick de Mars <pwdemars@gmail.com>
2021-04-11 11:12:38 +0100

    Added experiment 106 (UNTESTED) to new folder

---
+++ local_scripts/106_milp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# set today's date to use as save directory
+date=$(date +"%y-%m-%d")
+
+for g in 10 20 30 ;
+do python $HOME/pglib-uc/solve_and_test.py --save_dir results/${date}_106/milp_g${g}_perfect \
+						 --env_params_fn $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
+						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}gen/30min \
+						 --num_samples 1000 \
+						 --perfect_forecast true ;
+done 					
+
+for g in 10 20 30 ;
+do python $HOME/pglib-uc/solve_and_test.py --save_dir results/${date}_106/milp_g${g}_4sigma \
+						 --env_params_fn $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
+						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}gen/30min \
+						 --num_samples 1000 \
+						 --reserve_sigma 4 ;
+done 					
