a01028459c54427e0df023bf056c48636c54c338
Patrick de Mars <pwdemars@gmail.com>
2021-04-13 15:27:47 +0100

    Increased time budget by 100s for exp202 and exp203

--- hpc_scripts/203-rolling_horizon.sh
+++ hpc_scripts/203-rolling_horizon.sh
@@ -9,7 +9,7 @@ tree_search_func_name="ida_star"
 for g in 10 20 30; 
   do for t in 60;
     do for heuristic_method in "advanced_priority_list";
-      do let secs=$t*48*2 && \
+      do let secs=$t*48*3+100 && \
          time=$(date -d@$secs -u +%H:%M:%S) && \
          qsub -l h_rt=$time submit_rolling_tree_search.sh $HOME/Scratch/results/${date}_203/rolling_guided_${tree_search_func_name}/g${g}/g${g}_t${t}_p${rho}_${heuristic_method} \
 														 $HOME/AISO_HPC/best_policies/g${g}/params.json \
