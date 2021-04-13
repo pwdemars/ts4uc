8f90e07187380271a5641c27d46a59b8c91696a0
Patrick de Mars <pwdemars@gmail.com>
2021-04-13 18:35:28 +0100

    Further increasing time allocation

--- hpc_scripts/202-anytime_ida_star.sh
+++ hpc_scripts/202-anytime_ida_star.sh
@@ -9,7 +9,7 @@ tree_search_func_name="ida_star"
 for g in 10 20 30; 
   do for t in 1 2 5 10 30 60;
     do for heuristic_method in "advanced_priority_list";
-      do let secs=$t*48*3+100 && \
+      do let secs=$t*48*3+500 && \
          time=$(date -d@$secs -u +%H:%M:%S) && \
          qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_202/guided_${tree_search_func_name}/g${g}/feb4_g${g}_t${t}_p${rho}_${heuristic_method} \
 														 $HOME/AISO_HPC/best_policies/g${g}/params.json \
