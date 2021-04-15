6ac8348055f9f75339a7f704b7ffea7093cb81b3
Patrick de Mars <pwdemars@gmail.com>
2021-04-15 18:04:50 +0100

    Running H=2 as well in exp201

--- hpc_scripts/201-heuristic_comparison.sh
+++ hpc_scripts/201-heuristic_comparison.sh
@@ -6,7 +6,7 @@ date=$(date +"%y-%m-%d")
 # Different for 30 gens! 
 for g in 10 20 30;
 	do for tree_search_func_name in a_star;
-		do for H in 4 ; 
+		do for H in 2 4 ; 
 			do for rho in 05;
 				do for heuristic_method in "check_lost_load" "priority_list" "advanced_priority_list" "none";
 					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_201/guided_${tree_search_func_name}/g${g}/feb4_g${g}_h${H}_p${rho}_${heuristic_method} \
