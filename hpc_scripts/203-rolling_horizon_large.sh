7906ebf73da6512e46e66d0ee7b0e362224e261f
Patrick de Mars <pwdemars@gmail.com>
2021-04-26 15:56:18 +0100

    Changed save_dir to not overwrite the old exp203

--- hpc_scripts/203-rolling_horizon_large.sh
+++ hpc_scripts/203-rolling_horizon_large.sh
@@ -13,7 +13,7 @@ for g in 10 20 30;
     do for heuristic_method in "advanced_priority_list";
       do let secs=$t*48*3+100 && \
          time=$(date -d@$secs -u +%H:%M:%S) && \
-         qsub -l h_rt=$time submit_rolling_tree_search_scenarios.sh $HOME/Scratch/results/${date}_203/rolling_guided_${tree_search_func_name}/g${g}/g${g}_t${t}_p${rho}_${heuristic_method} \
+         qsub -l h_rt=$time submit_rolling_tree_search_scenarios.sh $HOME/Scratch/results/${date}_203_scenarios/rolling_guided_${tree_search_func_name}/g${g}/g${g}_t${t}_p${rho}_${heuristic_method} \
 														 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 														 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
 														 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
