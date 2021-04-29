1878a5df9f49346371e1575905d0fde93d0ced93
Patrick de Mars <pwdemars@gmail.com>
2021-04-29 13:58:01 +0100

    Renamed save directories

--- hpc_scripts/201-heuristic_comparison.sh
+++ hpc_scripts/201-heuristic_comparison.sh
@@ -9,7 +9,7 @@ for g in 10 20 30;
 		do for H in 2 4 ; 
 			do for rho in 05;
 				do for heuristic_method in "check_lost_load" "simple_priority_list" "advanced_priority_list" "none";
-					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_201/guided_${tree_search_func_name}/g${g}/feb4_g${g}_h${H}_p${rho}_${heuristic_method} \
+					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_201/guided_${tree_search_func_name}/g${g}/g${g}_h${H}_p${rho}_${heuristic_method} \
 																 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 																 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
 																 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
@@ -29,7 +29,7 @@ for g in 10 20 30;
 		do for H in 6 8; 
 			do for rho in 05;
 				do for heuristic_method in "advanced_priority_list";
-					do qsub -l h_rt=16:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_201/guided_${tree_search_func_name}/g${g}/feb4_g${g}_h${H}_p${rho}_${heuristic_method} \
+					do qsub -l h_rt=16:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_201/guided_${tree_search_func_name}/g${g}/g${g}_h${H}_p${rho}_${heuristic_method} \
 																 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 																 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
 																 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
--- hpc_scripts/202-anytime_ida_star.sh
+++ hpc_scripts/202-anytime_ida_star.sh
@@ -11,7 +11,7 @@ for g in 10 20 30;
     do for heuristic_method in "advanced_priority_list" "none";
       do let secs=$t*48*3+500 && \
          time=$(date -d@$secs -u +%H:%M:%S) && \
-         qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_202/guided_${tree_search_func_name}/g${g}/feb4_g${g}_t${t}_p${rho}_${heuristic_method} \
+         qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_202/guided_${tree_search_func_name}/g${g}/g${g}_t${t}_p${rho}_${heuristic_method} \
 														 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 														 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
 														 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
