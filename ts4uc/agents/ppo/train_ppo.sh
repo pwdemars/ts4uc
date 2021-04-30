e5be766cf63d3b88f85721ccb78716df95c4142c
Patrick de Mars <pwdemars@gmail.com>
2021-04-30 17:46:52 +0100

    Renamed

---
+++ ts4uc/agents/ppo/train_ppo.sh
@@ -0,0 +1,11 @@
+python $HOME/ts4uc/ts4uc/agents/ppo/train.py \
+       --save_dir results/ppo_tests/v9 \
+       --num_gen 5 \
+       --timesteps 500000 \
+       --workers 4 \
+       --steps_per_epoch 1000 \
+       --entropy_coef 0.05 \
+	   --update_epochs 10 \
+	   --clip_ratio 0.1 \
+	   --ac_learning_rate 0.00003 \
+	   --cr_learning_rate 0.0003 \
