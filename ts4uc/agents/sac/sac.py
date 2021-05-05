a1bf950b56f06339a613b391ccb7bcc386eefce2
Patrick de Mars <pwdemars@gmail.com>
2021-05-05 15:47:28 +0100

    Initial commit of soft actor-critic

---
+++ ts4uc/agents/sac/sac.py
@@ -0,0 +1,289 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+import time
+import numpy as np
+import scipy.signal as signal
+
+from ts4uc.helpers import process_observation
+
+from rl4uc import processor
+
+def polyak_update(params, target_params, tau):
+    """
+    Perform a Polyak average update on ``target_params`` using ``params``:
+    target parameters are slowly updated towards the main parameters.
+    ``tau``, the soft update coefficient controls the interpolation:
+    ``tau=1`` corresponds to copying the parameters to the target ones whereas nothing happens when ``tau=0``.
+    The Polyak update is done in place, with ``no_grad``, and therefore does not create intermediate tensors,
+    or a computation graph, reducing memory cost and improving performance.  We scale the target params
+    by ``1-tau`` (in-place), add the new weights, scaled by ``tau`` and store the result of the sum in the target
+    params (in place).
+    See https://github.com/DLR-RM/stable-baselines3/issues/93
+
+    :param params: parameters to use to update the target params
+    :param target_params: parameters to update
+    :param tau: the soft update coefficient ("Polyak update", between 0 and 1)
+    """
+    with torch.no_grad():
+        # zip does not raise an exception if length of parameters does not match.
+        for param, target_param in zip(params, target_params):
+            target_param.data.mul_(1 - tau)
+            torch.add(target_param.data, param.data, alpha=tau, out=target_param.data)
+
+class Critic(nn.Module):
+    def __init__(self, env, **kwargs):
+        super(Critic, self).__init__()
+
+        self.n_in = kwargs.get('obs_size') + 1 
+        self.num_nodes = int(kwargs.get('num_nodes'))
+        self.num_layers = int(kwargs.get('num_layers'))
+
+        self.in_layer = nn.Linear(self.n_in, self.num_nodes)
+        self.layers = nn.ModuleList([nn.Linear(self.num_nodes, self.num_nodes) for i in range(self.num_layers)])
+        self.out_layer = nn.Linear(self.num_nodes, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=kwargs.get('cr_learning_rate'))
+
+    def forward(self, obs, act):
+        x = torch.cat([obs, act], dim=1)
+        x = self.in_layer(x)
+        x = F.relu(x)
+        for l in self.layers: 
+            x = l(x)
+            x = F.relu(x)
+        return self.out_layer(x)
+
+class SACAgent(nn.Module):
+
+    def __init__(self, env, **kwargs):
+
+        super(SACAgent, self).__init__()
+
+        self.dispatch_freq_mins = env.dispatch_freq_mins
+        self.forecast_horizon = int(kwargs.get('forecast_horizon_hrs') * 60 / self.dispatch_freq_mins)
+        self.env = env
+        
+        if kwargs.get('observation_processor') == 'LimitedHorizonProcessor':
+            self.obs_processor = processor.LimitedHorizonProcessor(env, forecast_horizon=self.forecast_horizon)
+        elif kwargs.get('observation_processor') == 'DayAheadProcessor':
+            self.obs_processor = processor.DayAheadProcessor(env, forecast_errors=kwargs.get('observe_forecast_errors', False))
+        else:
+            raise ValueError(f"{kwargs.get('observation_processor')} is not a valid observation processor")
+        
+        self.n_in = 2*env.num_gen + self.obs_processor.obs_size
+        
+        self.num_nodes = int(kwargs.get('num_nodes'))
+        self.num_layers = int(kwargs.get('num_layers'))
+
+        n_critics = 2
+        self.critics = [Critic(env, obs_size=self.n_in, **kwargs) for i in range(n_critics)]
+        self.critics_target = []
+        for cr in self.critics:
+            cr_target = Critic(env, obs_size=self.n_in, **kwargs)
+            cr_target.load_state_dict(cr.state_dict())
+            self.critics_target.append(cr_target)
+
+        self.in_ac = nn.Linear(self.n_in, self.num_nodes)
+        self.ac_layers = nn.ModuleList([nn.Linear(self.num_nodes, self.num_nodes) for i in range(self.num_layers)])
+        self.output_ac = nn.Linear(self.num_nodes, 2)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.test_seed = kwargs.get('test_seed')
+
+        self.gradient_steps = 2
+        self.minibatch_size = None
+        self.ent_coef = "auto"
+        self.target_entropy = 0.2
+
+        self.optimizer = optim.Adam(self.parameters(), lr=kwargs.get('ac_learning_rate'))
+
+
+        # Default initial value of ent_coef when learned
+        init_value = 1.0
+        if "_" in self.ent_coef:
+            init_value = float(self.ent_coef.split("_")[1])
+            assert init_value > 0.0, "The initial value of ent_coef must be greater than 0"
+
+        # Note: we optimize the log of the entropy coeff which is slightly different from the paper
+        # as discussed in https://github.com/rail-berkeley/softlearning/issues/37
+        self.log_ent_coef = torch.log(torch.ones(1, device=self.device) * init_value).requires_grad_(True)
+        self.ent_coef_optimizer = torch.optim.Adam([self.log_ent_coef], lr=0.001)
+        self.target_update_interval = 1
+        self.gamma = 0.99
+        self.tau = 0.9
+
+    def get_action_scores(self, x):
+        x = self.in_ac(x)
+        x = F.relu(x)
+        for l in self.ac_layers:
+            x = l(x)
+            x = F.relu(x)
+        action_scores = self.output_ac(x)
+        return action_scores        
+
+    def forward_ac(self, x):
+        action_scores = self.get_action_scores(x)
+        pi = Categorical(probs=F.softmax(action_scores, dim=action_scores.ndim-1))
+        return pi
+
+
+    def obs_with_constraints(self, env, obs):
+
+        x = self.obs_processor.process(obs)
+ 
+        # Init action with constraints
+        action = np.zeros(env.num_gen, dtype=int)
+        action[np.where(env.must_on)[0]] = 1
+        
+        # Determine constrained gens
+        constrained_gens = np.where(np.logical_or(env.must_on, env.must_off))
+        unconstrained_gens = np.delete(np.arange(env.num_gen), constrained_gens)
+        
+        # Append action
+        x = np.append(action, x)
+        
+        # Append one-hot encoding
+        x = np.append(np.zeros(env.num_gen), x)
+
+        # Convert state to tensor        
+        x = torch.as_tensor(x).float().to(self.device)
+
+        return x, action
+
+
+    def generate_action(self, env, obs):
+        """
+        1. Determine constrained generators, init action and concatenate onto state
+        2. Concatenate one-hot encoding onto state
+        3. For all unconstrained generators:
+            Forward pass with x[i]=1 (one-hot encoding).
+            Sample action from softmax, change action[i]
+            Change part of action part of state 
+        """
+        x, action = self.obs_with_constraints(env, obs)
+        
+        # Init log_probs
+        log_probs = []
+        sub_obs = []
+        sub_acts = []
+        
+        # Init entropys
+        entropys = []
+
+        constrained_gens = np.where(np.logical_or(env.must_on, env.must_off))
+        unconstrained_gens = np.delete(np.arange(env.num_gen), constrained_gens)
+        
+        for idx in unconstrained_gens:
+            
+            x_g = x.clone()
+            
+            # Set one hot encoding
+            x_g[idx] = 1
+            
+            # Forward pass
+            pi = self.forward_ac(x_g)
+            
+            # Sample action
+            a = pi.sample()
+            
+            # Update log_prob
+            log_prob = pi.log_prob(a)
+            log_probs.append(log_prob)
+            entropys.append(pi.entropy())
+            
+            # Add to data buffer 
+            sub_obs.append(x_g)
+            sub_acts.append(a)
+
+            # Change action
+            action[idx] = a
+            
+            # Change state tensor x 
+            x[env.num_gen+idx] = a
+        
+        return action, sub_obs, sub_acts, log_probs
+
+    def update(self, buf, pi_optimizer, q_optimizer):
+
+        data = buf.get()
+        ent_coef_losses, ent_coefs = [], []
+        actor_losses, critic_losses = [], []
+
+        for gradient_step in range(self.gradient_steps):
+
+            if self.minibatch_size is not None:
+                minibatch = self.get_minibatch(data, buf.num_used)
+            else:
+                minibatch = data
+
+            obs, act, rew, next_obs, dones = minibatch['obs'], minibatch['act'], minibatch['rew'], minibatch['next_obs'], minibatch['rew']
+
+            pi = self.forward_ac(obs)
+            log_prob = pi.log_prob(act)
+
+            # Important: detach the variable from the graph
+            # so we don't change it with other losses
+            # see https://github.com/rail-berkeley/softlearning/issues/60
+            ent_coef = torch.exp(self.log_ent_coef.detach())
+            ent_coef_loss = -(self.log_ent_coef * (log_prob + self.target_entropy).detach()).mean()
+            ent_coef_losses.append(ent_coef_loss.item())
+
+            ent_coefs.append(ent_coef.item())
+
+            if ent_coef_loss is not None:
+                self.ent_coef_optimizer.zero_grad()
+                ent_coef_loss.backward()
+                self.ent_coef_optimizer.step()
+
+            with torch.no_grad():
+                # Select action according to policy
+                next_pi = self.forward_ac(next_obs)
+                next_actions = next_pi.sample()
+                next_log_prob = next_pi.log_prob(next_actions)
+                # Compute the next Q values: min over all critics targets
+                next_q_values = [critic.forward(obs, act) for critic in self.critics_target]
+                next_q_values = torch.cat(next_q_values, dim=1)
+                # next_q_values = torch.cat(self.critic_target(next_obs, next_actions), dim=1)
+                next_q_values, _ = torch.min(next_q_values, dim=1, keepdim=True)
+                # add entropy term
+                next_q_values = next_q_values - ent_coef * next_log_prob.reshape(-1, 1)
+                # td error + entropy term
+                target_q_values = rew + (1 - dones) * self.gamma * next_q_values
+
+            # Get current Q-values estimates for each critic network
+            # using action from the replay buffer
+            current_q_values = [critic.forward(obs, act) for critic in self.critics]
+
+            # Compute critic loss
+            critic_loss = 0.5 * sum([F.mse_loss(current_q, target_q_values) for current_q in current_q_values])
+            critic_losses.append(critic_loss.item())
+
+
+            # Optimize the critic
+            [cr.optimizer.zero_grad() for cr in self.critics]
+            critic_loss.backward()
+            [cr.optimizer.step() for cr in self.critics]
+
+            # Compute actor loss
+            # Alternative: actor_loss = th.mean(log_prob - qf1_pi)
+            # Mean over all critic networks
+            q_values_pi = [critic.forward(obs, act) for critic in self.critics]
+            q_values_pi = torch.cat(q_values_pi, dim=1)
+            min_qf_pi, _ = torch.min(q_values_pi, dim=1, keepdim=True)
+            actor_loss = (ent_coef * log_prob - min_qf_pi).mean()
+            actor_losses.append(actor_loss.item())
+
+            # Optimize the actor
+            pi_optimizer.zero_grad()
+            actor_loss.backward()
+            pi_optimizer.step()
+
+            # Update target networks
+            if gradient_step % self.target_update_interval == 0:
+                for cr, target_cr in zip(self.critics, self.critics_target):
+                    polyak_update(cr.parameters(), target_cr.parameters(), self.tau)
+
---
+++ ts4uc/agents/sac/train.py
@@ -0,0 +1,335 @@
+import torch
+import torch.optim as optim
+import torch.multiprocessing as mp
+import numpy as np
+import time
+import scipy.signal as signal
+import copy
+from collections import namedtuple
+
+from rl4uc.environment import make_env
+from ts4uc.agents.sac import sac
+from ts4uc.agents.ppo.train import Logger
+from ts4uc import helpers
+import pandas as pd 
+import os
+
+device = "cpu"
+mp.set_start_method('spawn', True)
+
+MsgUpdateRequest = namedtuple('MsgUpdateRequest', ['agent', 'update'])
+
+class ReplayBuffer:
+
+    def __init__(self, max_size, obs_dim=None):
+
+        self.max_size = max_size
+        self.obs_buf = torch.zeros(
+            (max_size, obs_dim)).to(device).share_memory_()
+        self.act_buf = torch.zeros(
+            max_size, 1).to(device).share_memory_()
+        self.rew_buf = torch.zeros(
+            max_size, 1).to(device).share_memory_()
+        self.next_obs_buf = torch.zeros(
+            max_size, obs_dim).to(device).share_memory_()
+        self.done_buf = torch.zeros(
+            max_size, 1).to(device).share_memory_() 
+        self.num_used = torch.zeros(1, dtype=int).share_memory_()
+
+    def reset(self):
+        self.num_used.zero_()
+
+    def store(self, obs=None, act=None, rew=None, next_obs=None, done=None):
+
+        n = self.num_used.item() % self.max_size
+
+        if obs != None:
+            self.obs_buf[n] = obs
+        if act != None:
+            self.act_buf[n] = act
+        if rew != None:
+            self.rew_buf[n] = rew
+        if next_obs != None:
+            self.next_obs_buf[n] = next_obs
+        if done != None:
+            self.done_buf[n] = done
+
+        self.num_used += 1
+
+
+
+    def get(self):
+
+        data = dict(obs=self.obs_buf, act=self.act_buf,
+                    rew=self.rew_buf, next_obs=self.next_obs_buf,
+                    done=self.done_buf)
+
+        return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in data.items()}
+
+class Worker(mp.Process):
+	"""
+	Worker class
+	"""
+	def __init__(self, worker_id, env, pipe, policy, gamma, lam, 
+				num_epochs, steps_per_epoch, buf, logger):
+
+		mp.Process.__init__(self, name=worker_id)
+
+
+		self.worker_id = worker_id
+		self.policy = policy
+		self.env = copy.deepcopy(env)
+		self.gamma = gamma
+		self.lam = lam
+		self.pipe = pipe
+		self.num_epochs = num_epochs
+		self.steps_per_epoch = steps_per_epoch
+		self.buf = buf
+		self.logger = logger
+
+	def run(self):
+
+		for epoch in range(self.num_epochs):
+
+			all_ep_rewards, all_ep_timesteps = self.run_epoch()
+
+			self.logger.log(reward=np.mean(all_ep_rewards),
+							std_reward=np.std(all_ep_rewards),
+							q25_reward=np.quantile(all_ep_rewards, 0.25),
+							q75_reward=np.quantile(all_ep_rewards, 0.75),
+							timesteps=np.mean(all_ep_timesteps),
+							std_timesteps=np.std(all_ep_timesteps),
+							q25_timesteps=np.quantile(all_ep_timesteps, 0.25),
+							q75_timesteps=np.quantile(all_ep_timesteps, 0.75),
+							worker_id=int(self.worker_id),
+							epoch=epoch)
+
+			msg = MsgUpdateRequest(int(self.worker_id), True)
+			self.pipe.send(msg)
+			msg = self.pipe.recv()
+
+	def run_epoch(self):
+
+		all_ep_timesteps = []
+		all_ep_rewards = []
+
+		acts = []
+		obss = []
+		next_obss = []
+		rews = []
+		dones = []
+
+		while sum(all_ep_timesteps) <= self.steps_per_epoch: 
+
+			obs = self.env.reset() 
+
+			ep_timesteps = 0
+			ep_reward = 0
+			unscaled_ep_rewards = []
+			ep_rewards = []
+			ep_values = []
+
+			done = False
+
+			while not done:
+				# Choose action with actor
+				a, sub_obs, sub_acts, _ = self.policy.generate_action(self.env, obs)
+
+				# Step environment 
+				obs, reward, done = self.env.step(a)
+
+				# Record unscaled reward
+				unscaled_ep_rewards.append(reward)
+
+				# Transform the reward
+				reward = 1+reward/-self.env.min_reward
+				# reward = (reward - self.policy.mean_reward) / self.policy.std_reward
+				reward = reward.clip(-10, 10)
+
+				# Update episode rewards and timesteps
+				ep_timesteps += 1
+
+				# for i in range(len(a)):
+				# 	acts.append(a[i])
+				# 	obss.append(sub_obs[i])
+				# 	rews.append(reward)
+				# 	if done and ((i+1) == len(a)):
+				# 		last_obs = self.policy.obs_with_constraints(env, obs)
+				# 		next_obss.append(last_obs)
+				# 	else:
+				# 		next_obss.append(sub_obs[i+1])
+				# 	dones.append(done)
+				for i in range(len(sub_acts)):
+
+					if ((i+1) == len(sub_acts)):
+						next_obs, _ = self.policy.obs_with_constraints(self.env, obs)
+					else:
+						next_obs = sub_obs[i+1]
+						
+
+					self.buf.store(obs=sub_obs[i],
+								   act=sub_acts[i],
+								   rew=reward,
+								   next_obs=next_obs,
+								   done=done)
+
+			all_ep_rewards.append(sum(unscaled_ep_rewards))
+			all_ep_timesteps.append(ep_timesteps)
+
+
+		return all_ep_rewards, all_ep_timesteps
+
+
+def train(save_dir,
+		  timesteps, 
+		  num_workers, 
+		  steps_per_epoch, 
+		  env_params, 
+		  policy_params,
+		  gamma,
+		  lam):
+
+	# Total number of epochs (updates)
+	num_epochs = int(timesteps / steps_per_epoch)
+
+	# Number of timesteps each worker should gather per epoch
+	worker_steps_per_epoch = int(steps_per_epoch / num_workers)
+
+
+	env = make_env(**env_params)
+	policy = sac.SACAgent(env, **policy_params).share_memory()
+
+	pi_optimizer = optim.Adam(policy.parameters(), lr=policy_params.get('ac_learning_rate'))
+	q_optimizer = optim.Adam(policy.parameters(), lr=policy_params.get('cr_learning_rate'))
+
+	# The actor buffer will typically take more entries than the critic buffer,
+	# because it records sub-actions. Hence there is usually more than one entry
+	# per timestep. Here we set the size to be the max possible.
+	buf = ReplayBuffer(max_size=num_workers*steps_per_epoch*env.num_gen, obs_dim=policy.n_in)
+
+	logger = Logger(num_epochs, num_workers, steps_per_epoch)
+
+
+	# Worker update requests
+	update_request = [False]*num_workers
+	epoch_counter = 0
+
+	workers = []
+	pipes = []
+
+	for worker_id in range(num_workers):
+		p_start, p_end = mp.Pipe()
+		worker = Worker(worker_id=str(worker_id),
+						env=env,
+						policy=policy,
+						pipe=p_end,
+						buf=buf,
+						logger=logger,
+						num_epochs=num_epochs,
+						steps_per_epoch=worker_steps_per_epoch,
+						gamma=gamma,
+						lam=lam)
+		worker.start()
+		workers.append(worker)
+		pipes.append(p_start)
+
+	start_time = time.time()
+
+	# starting training loop
+	while epoch_counter < num_epochs:
+		for i, conn in enumerate(pipes):
+			if conn.poll():
+				msg = conn.recv()
+
+				# if agent is waiting for network update
+				if type(msg).__name__ == "MsgUpdateRequest":
+					update_request[i] = True
+					if False not in update_request:
+
+						print("Epoch: {}".format(epoch_counter))
+						print("Updating")
+
+
+						policy.update(buf,  pi_optimizer, q_optimizer)
+
+						epoch_counter += 1
+						update_request = [False]*num_workers
+						msg = epoch_counter
+
+						# periodically save the logs
+						if (epoch_counter + 1) % 10 == 0:
+							logger.save_to_csv(os.path.join(save_dir, 'logs.csv'))
+
+						# send to signal subprocesses to continue
+						for pipe in pipes:
+							pipe.send(msg)
+
+	time_taken = time.time() - start_time
+
+	logger.save_to_csv(os.path.join(save_dir, 'logs.csv'))
+	torch.save(policy.state_dict(), os.path.join(save_dir, 'ac_final.pt'))
+
+	# Record training time
+	with open(os.path.join(save_dir, 'time_taken.txt'), 'w') as f:
+		f.write(str(time_taken) + '\n')
+
+
+if __name__ == "__main__":
+	
+	import argparse
+	import json
+
+	
+	parser = argparse.ArgumentParser(description='Train PPO agent')
+	parser.add_argument('--save_dir', type=str, required=True)
+	parser.add_argument('--workers', type=int, required=False, default=1)
+	parser.add_argument('--num_gen', type=str, required=True)
+	parser.add_argument('--timesteps', type=int, required=True)
+	parser.add_argument('--steps_per_epoch', type=int, required=True)
+
+	# The following params will be used to setup the PPO agent
+	parser.add_argument('--ac_learning_rate', type=float, required=False, default=3e-05)
+	parser.add_argument('--cr_learning_rate', type=float, required=False, default=3e-04)
+	parser.add_argument('--num_layers', type=int, required=False, default=3)
+	parser.add_argument('--num_nodes', type=int, required=False, default=32)
+	parser.add_argument('--entropy_coef', type=float, required=False, default=0.01)
+	parser.add_argument('--clip_ratio', type=float, required=False, default=0.1)
+	parser.add_argument('--forecast_horizon_hrs', type=int, required=False, default=12)
+	parser.add_argument('--credit_assignment_1hr', type=int, required=False, default=0.9)
+	parser.add_argument('--minibatch_size', type=int, required=False, default=None)
+	parser.add_argument('--update_epochs', type=int, required=False, default=4)
+	parser.add_argument('--observation_processor', type=str, required=False, default='LimitedHorizonProcessor')
+
+
+	args = parser.parse_args()
+
+	# Make save dir
+	os.makedirs(args.save_dir, exist_ok=True)
+
+	# Load policy params and save them to the local directory. 
+	policy_params = vars(args)
+	with open(os.path.join(args.save_dir, 'params.json'), 'w') as fp:
+		fp.write(json.dumps(policy_params, sort_keys=True, indent=4))
+	
+	# Load the env params and save them to save_dir
+	env_params = helpers.retrieve_env_params(args.num_gen)
+	with open(os.path.join(args.save_dir, 'env_params.json'), 'w') as fp:
+		fp.write(json.dumps(env_params, sort_keys=True, indent=4))
+
+	# Calculate gamma 
+	gamma = helpers.calculate_gamma(policy_params['credit_assignment_1hr'], env_params['dispatch_freq_mins'])
+
+	# Lambda for GAE 
+	lam = 0.95
+
+	train(save_dir=args.save_dir,
+		  timesteps=args.timesteps,
+		  num_workers=args.workers,
+		  steps_per_epoch=args.steps_per_epoch,
+		  env_params=env_params,
+		  policy_params=policy_params,
+		  gamma=gamma,
+		  lam=lam)
+
+
+
