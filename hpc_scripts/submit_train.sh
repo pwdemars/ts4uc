f2c0d6e9293bf26f1be7e99b75083c756c3002d3
Patrick de Mars <pwdemars@gmail.com>
2021-05-06 17:56:01 +0100

    Corrected parameter order

--- hpc_scripts/submit_train.sh
+++ hpc_scripts/submit_train.sh
@@ -7,7 +7,7 @@
 save_dir=$1
 num_gen=$2
 workers=$3
-num_epochs=$4
+epochs=$4
 entropy_coef=$5
 
 module load python3/3.7
@@ -18,6 +18,6 @@ mkdir -p $HOME/Scratch/results/$save_dir
 cd $TMPDIR
 
 $HOME/ts4uc/hpc_scripts/train.sh \
-    $save_dir $num_gen $num_epochs $workers $entropy_coef
+    $save_dir $num_gen $workers $epochs $entropy_coef
 
 tar zcvf $HOME/Scratch/results/$save_dir/results.tar.gz $TMPDIR
--- hpc_scripts/train.sh
+++ hpc_scripts/train.sh
@@ -2,20 +2,20 @@
 
 save_dir=$1
 num_gen=$2
-num_procs=$3
-num_epochs=$4
+workers=$3
+epochs=$4
 entropy_coef=$5
 
 python $HOME/ts4uc/ts4uc/agents/ppo_async/train.py \
        --save_dir $save_dir \
        --num_gen $num_gen \
-       --epochs $num_epochs \
-       --workers $num_procs \
+       --epochs $epochs \
+       --workers $workers \
        --entropy_coef $entropy_coef \
        --ac_learning_rate 3e-05 \
        --cr_learning_rate 3e-04 \
        --num_layers 3 \
        --num_nodes 64 \
        --clip_ratio 0.1 \
-       --buffer_size 2000
+       --buffer_size 2000 \
        --seed 10
