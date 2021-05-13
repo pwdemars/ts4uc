497d187050d812de60579266519f6a61e3acce07
Patrick de Mars <pwdemars@gmail.com>
2021-05-13 08:54:53 +0100

    Adding forecast horizon and credit assignment to train.sh

--- hpc_scripts/train.sh
+++ hpc_scripts/train.sh
@@ -23,5 +23,6 @@ python $HOME/ts4uc/ts4uc/agents/ppo_async/train.py \
        --num_layers $num_layers \
        --num_nodes $num_nodes \
        --buffer_size 2000 \
-       --credit_assignment_1hr 0.8 \
+       --credit_assignment_1hr 0.9 \
+       --forecast_horizon_hrs 12 \
        --seed 10
