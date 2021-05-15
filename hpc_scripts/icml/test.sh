05e47fee3801ce2e0dc29f98668ea4e24f4af60d
Patrick de Mars <pwdemars@gmail.com>
2021-05-15 12:10:02 +0100

    Fixing a variable save_dir_root

--- hpc_scripts/icml/test.sh
+++ hpc_scripts/icml/test.sh
@@ -2,16 +2,19 @@
 
 # set date that policies were trained. will be used for tests too 
 date=$1
+save_dir_root=$HOME/Scratch/results/${date}_icml
+
+echo "Reading policies from and writing results to: ${save_dir_root}"
 
 for g in 10 20 30;
 	do for tree_search_func_name in a_star;
 		do for H in 2 4 ; 
 			do for rho in 05;
 				do for heuristic_method in "advanced_priority_list" "none";
-					do qsub -l h_rt=12:00:00 ../submit_tree_search.sh $HOME/Scratch/results/${date}_icml/test/g${g}/h${H}_p${rho}_${heuristic_method} \
-																 $HOME/Scratch/results/${date}_icml/train/g${g}/params.json \
+					do qsub -l h_rt=12:00:00 ../submit_tree_search.sh ${save_dir_root}/test/g${g}/h${H}_p${rho}_${heuristic_method} \
+																 ${save_dir_root}/train/g${g}/params.json \
 																 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
-																 $HOME/Scratch/results/${date}_icml/train/g${g}/ac_final.pt \
+																 ${save_dir_root}/train/g${g}/ac_final.pt \
 																 ${H} \
 																 0.${rho} \
 																 ${tree_search_func_name} \
@@ -28,10 +31,10 @@ for g in 10 20 30;
 		do for H in 6 8; 
 			do for rho in 05;
 				do for heuristic_method in "advanced_priority_list";
-					do qsub -l h_rt=16:00:00 ../submit_tree_search.sh $HOME/Scratch/results/${date}_icml/test/g${g}/h${H}_p${rho}_${heuristic_method} \
-																 $HOME/Scratch/results/${date}_icml/train/g${g}/params.json \
+					do qsub -l h_rt=16:00:00 ../submit_tree_search.sh ${save_dir_root}/test/g${g}/h${H}_p${rho}_${heuristic_method} \
+																 ${save_dir_root}/train/g${g}/params.json \
 																 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
-																 $HOME/Scratch/results/${date}_icml/train/g${g}/ac_final.pt \
+																 ${save_dir_root}/train/g${g}/ac_final.pt \
 																 ${H} \
 																 0.${rho} \
 																 ${tree_search_func_name} \
