34cf5ffa9c099e94213ca3e7b23b1d3255a92db1
Patrick de Mars <pwdemars@gmail.com>
2021-05-25 11:54:12 +0100

    Reducing training epochs and time

--- hpc_scripts/icml/train.sh
+++ hpc_scripts/icml/train.sh
@@ -6,12 +6,12 @@ date=$(date +"%y-%m-%d")
 # 10 gen
 num_gen=10
 workers=8
-epochs=200000
-hrs=24
+epochs=100000
+hrs=12
 entropy_coef=0.05
 clip_ratio=0.1
-ac_lr=1e-05
-cr_lr=1e-04
+ac_lr=3e-05
+cr_lr=3e-04
 ac_arch="100,50,25"
 cr_arch="64,64"
 for c in 0 1;
@@ -23,12 +23,12 @@ do qsub -pe smp $workers -l h_rt=${hrs}:00:00 ../submit_train.sh \
 # 20 gen
 num_gen=20
 workers=8
-epochs=400000
-hrs=36
+epochs=200000
+hrs=18
 entropy_coef=0.001
 clip_ratio=0.1
-ac_lr=1e-05
-cr_lr=1e-04
+ac_lr=3e-05
+cr_lr=3e-04
 ac_arch="64,64"
 cr_arch="100,50,25"
 for c in 0 1;
@@ -39,12 +39,12 @@ for c in 0 1;
 # 30 gen
 num_gen=30
 workers=8
-epochs=600000
-hrs=48
+epochs=300000
+hrs=24
 entropy_coef=0.0
 clip_ratio=0.1
-ac_lr=1e-05
-cr_lr=1e-04
+ac_lr=3e-05
+cr_lr=3e-04
 ac_arch="64,64"
 cr_arch="400,300"
 for c in 0 1; 
