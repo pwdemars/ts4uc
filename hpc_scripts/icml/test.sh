011a25a4f6746d1a3a25a8c47c0daffbe0be6abe
Patrick de Mars <pwdemars@gmail.com>
2021-05-26 07:53:19 +0100

    Commented out A* search and added epoch variable
    Allows to train intermediate policies

--- hpc_scripts/icml/test.sh
+++ hpc_scripts/icml/test.sh
@@ -6,48 +6,8 @@ save_dir_root=$HOME/Scratch/results/${root}
 
 echo "Reading policies from and writing results to: ${save_dir_root}"
 
-# A* test
-for g in 10 20 30;
-	do for tree_search_func_name in a_star;
-		do for H in 2 4 ; 
-			do for rho in 05;
-				do for heuristic_method in "advanced_priority_list" "none";
-					do qsub -l h_rt=2:00:00 ../submit_tree_search.sh ${save_dir_root}/test/g${g}/h${H}_p${rho}_${heuristic_method} \
-																 ${save_dir_root}/train/g${g}/params.json \
-																 ${save_dir_root}/train/g${g}/env_params.json \
-																 ${save_dir_root}/train/g${g}/ac_final.pt \
-																 ${H} \
-																 0.${rho} \
-																 ${tree_search_func_name} \
-																 $HOME/ts4uc/data/hpc_params/input_day_ahead_g${g}.txt \
-																 $heuristic_method ; 
-				done ;
-			done ;
-		done ;
-	done ;
-done ;
-
-for g in 10 20 30;
-	do for tree_search_func_name in a_star;
-		do for H in 6 8; 
-			do for rho in 05;
-				do for heuristic_method in "advanced_priority_list";
-					do qsub -l h_rt=8:00:00 ../submit_tree_search.sh ${save_dir_root}/test/g${g}/h${H}_p${rho}_${heuristic_method} \
-																 ${save_dir_root}/train/g${g}/params.json \
-																 ${save_dir_root}/train/g${g}/env_params.json \
-																 ${save_dir_root}/train/g${g}/ac_final.pt \
-																 ${H} \
-																 0.${rho} \
-																 ${tree_search_func_name} \
-																 $HOME/ts4uc/data/hpc_params/input_day_ahead_g${g}.txt \
-																 $heuristic_method ; 
-				done ;
-			done ;
-		done ;
-	done ;
-done ;
-
 ## Anytime test
+epoch=final
 rho=05
 tree_search_func_name="ida_star"
 for g in 10 20 30; 
@@ -55,10 +15,10 @@ for g in 10 20 30;
     do for heuristic_method in "advanced_priority_list";
       do let secs=$t*48*3+500 && \
          time=$(date -d@$secs -u +%H:%M:%S) && \
-         qsub -l h_rt=$time ../submit_anytime_tree_search.sh ${save_dir_root}/test_anytime/g${g}/t${t}_p${rho}_${heuristic_method} \
+         qsub -l h_rt=$time ../submit_anytime_tree_search.sh ${save_dir_root}/test_anytime_${epoch}/g${g}/t${t}_p${rho}_${heuristic_method} \
 														 ${save_dir_root}/train/g${g}/params.json \
 														 ${save_dir_root}/train/g${g}/env_params.json \
-														 ${save_dir_root}/train/g${g}/ac_final.pt \
+														 ${save_dir_root}/train/g${g}/ac_${epoch}.pt \
 														 ${t} \
 														 0.${rho} \
 														 ${tree_search_func_name} \
@@ -67,3 +27,45 @@ for g in 10 20 30;
 	  done ;
 	done ;
   done ;
+
+# # A* test
+# for g in 10 20 30;
+# 	do for tree_search_func_name in a_star;
+# 		do for H in 2 4 ; 
+# 			do for rho in 05;
+# 				do for heuristic_method in "advanced_priority_list" "none";
+# 					do qsub -l h_rt=2:00:00 ../submit_tree_search.sh ${save_dir_root}/test/g${g}/h${H}_p${rho}_${heuristic_method} \
+# 																 ${save_dir_root}/train/g${g}/params.json \
+# 																 ${save_dir_root}/train/g${g}/env_params.json \
+# 																 ${save_dir_root}/train/g${g}/ac_final.pt \
+# 																 ${H} \
+# 																 0.${rho} \
+# 																 ${tree_search_func_name} \
+# 																 $HOME/ts4uc/data/hpc_params/input_day_ahead_g${g}.txt \
+# 																 $heuristic_method ; 
+# 				done ;
+# 			done ;
+# 		done ;
+# 	done ;
+# done ;
+
+# for g in 10 20 30;
+# 	do for tree_search_func_name in a_star;
+# 		do for H in 6 8; 
+# 			do for rho in 05;
+# 				do for heuristic_method in "advanced_priority_list";
+# 					do qsub -l h_rt=8:00:00 ../submit_tree_search.sh ${save_dir_root}/test/g${g}/h${H}_p${rho}_${heuristic_method} \
+# 																 ${save_dir_root}/train/g${g}/params.json \
+# 																 ${save_dir_root}/train/g${g}/env_params.json \
+# 																 ${save_dir_root}/train/g${g}/ac_final.pt \
+# 																 ${H} \
+# 																 0.${rho} \
+# 																 ${tree_search_func_name} \
+# 																 $HOME/ts4uc/data/hpc_params/input_day_ahead_g${g}.txt \
+# 																 $heuristic_method ; 
+# 				done ;
+# 			done ;
+# 		done ;
+# 	done ;
+# done ;
+
