4e2facc0108eea024298636be37460531207d3af
Patrick de Mars <pwdemars@gmail.com>
2021-06-10 13:23:23 +0100

    Set save_dir; 3 seeds; changed permissions

--- hpc_scripts/204-train_big.sh
+++ hpc_scripts/204-train_big.sh
@@ -15,6 +15,8 @@ cr_lr=1e-04
 ac_arch="128,64"
 cr_arch="128,64"
 buffer_size=10000
-qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh \
-     ${date}_101/g${num_gen} $HOME/ts4uc/data/day_ahead/${num_gen}gen/30min/env_params.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch $buffer_size
+for v in 1 2 3 ;
+do qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh \
+     ${date}_204/g${num_gen}_v${v} $HOME/ts4uc/data/day_ahead/${num_gen}gen/30min/env_params.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch $buffer_size ; 
+done
 
