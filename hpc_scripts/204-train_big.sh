c705a32dffc1034fd8a31430d0b70ceeda3cde6f
Patrick de Mars <pwdemars@gmail.com>
2021-06-10 13:50:48 +0100

    Two versions with different buffer_size

--- hpc_scripts/204-train_big.sh
+++ hpc_scripts/204-train_big.sh
@@ -6,7 +6,6 @@ date=$(date +"%y-%m-%d")
 # 100 gen
 num_gen=100
 workers=8
-epochs=500000
 hrs=48
 entropy_coef=0.05
 clip_ratio=0.1
@@ -14,9 +13,12 @@ ac_lr=1e-05
 cr_lr=1e-04
 ac_arch="128,64"
 cr_arch="128,64"
-buffer_size=10000
-for v in 1 2 3 ;
-do qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh \
-     ${date}_204/g${num_gen}_v${v} $HOME/ts4uc/data/day_ahead/${num_gen}gen/30min/env_params.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch $buffer_size ; 
-done
+buffer_size=5000
+epochs=300000
+qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh \
+     ${date}_204/g${num_gen}_v1 $HOME/ts4uc/data/day_ahead/${num_gen}gen/30min/env_params.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch $buffer_size
 
+buffer_size=10000
+epochs=150000
+qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh \
+     ${date}_204/g${num_gen}_v2 $HOME/ts4uc/data/day_ahead/${num_gen}gen/30min/env_params.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch $buffer_size
