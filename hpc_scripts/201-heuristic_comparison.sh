bfe61d92dc1636527ab322d3f7e02f7867c31f6c
Patrick de Mars <pwdemars@gmail.com>
2021-06-14 17:44:38 +0100

    Removed the H=2

--- hpc_scripts/201-heuristic_comparison.sh
+++ hpc_scripts/201-heuristic_comparison.sh
@@ -6,7 +6,7 @@ date=$(date +"%y-%m-%d")
 # Different for 30 gens! 
 for g in 10 20 30;
 	do for tree_search_func_name in a_star;
-		do for H in 2 4 6 8; 
+		do for H in 4 6 8; 
 			do for rho in 05;
 				do for heuristic_method in "simple_priority_list" "simple_priority_list_ED" "advanced_priority_list";
 					do qsub -l h_rt=12:00:00 submit_tree_search.sh $HOME/Scratch/results/${date}_201/guided_${tree_search_func_name}/g${g}/g${g}_h${H}_p${rho}_${heuristic_method} \
