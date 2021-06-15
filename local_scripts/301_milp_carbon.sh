7b5e12e548ad1f84f19432bfc3c718e66cb3bee1
Patrick de Mars <pwdemars@gmail.com>
2021-06-15 18:12:37 +0100

    Added experiments for robustness and carbon

---
+++ local_scripts/301_milp_carbon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set today's date to use as save directory
+date=$(date +"%y-%m-%d")
+
+for g in 10 20 30 ;
+do for c in 10 25 50 100 ; 
+do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_301/milp_g${g}_carbon${c}_4sigma \
+						 --env_params_fn $HOME/ts4uc/data/envs/carbon/${g}gen_carbon${c}.json \
+						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}gen/30min \
+						 --num_samples 1000 \
+						 --reserve_sigma 4 ;
+done ; 
+done
---
+++ local_scripts/302_milp_robust.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# set today's date to use as save directory
+date=$(date +"%y-%m-%d")
+
+for g in 10 20 30 ;
+do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_302/milp_g${g}_4sigma \
+						 --env_params_fn $HOME/ts4uc/data/envs/robustness/${g}gen.json \
+						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}gen/30min \
+						 --num_samples 1000 \
+						 --reserve_sigma 4 \
+						 --n_minus_one ;
+done 					
