c191035e2494f4064359e1dcd9dc41a6e18b9489
Patrick de Mars <pwdemars@gmail.com>
2021-06-25 14:45:19 +0100

    Correct save_dir

--- hpc_scripts/206-test_big.sh
+++ hpc_scripts/206-test_big.sh
@@ -12,7 +12,7 @@ for g in 100;
     do for heuristic_method in "advanced_priority_list";
       do let secs=$t*48*3+500 && \
          time=$(date -d@$secs -u +%H:%M:%S) && \
-         qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_205/guided_${tree_search_func_name}/g${g}/g${g}_t${t}_p${rho}_${heuristic_method}_e${epoch} \
+         qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_206/guided_${tree_search_func_name}/g${g}/g${g}_t${t}_p${rho}_${heuristic_method}_e${epoch} \
 														 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 														 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
 														 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
