9280689a240823240c78e34e0511a052e3cda08a
Patrick de Mars <pwdemars@gmail.com>
2021-07-16 08:17:08 +0100

    Added training of policies with carbon price

---
+++ hpc_scripts/303_train_carbon.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# set today's date to use as save directory
+date=$(date +"%y-%m-%d")
+
+# 10 gen
+num_gen=10
+workers=8
+epochs=100000
+hrs=12
+entropy_coef=0.05
+clip_ratio=0.1
+ac_lr=3e-05
+cr_lr=3e-04
+ac_arch="100,50,25"
+cr_arch="64,64"
+for c in 25 50;
+do qsub -pe smp $workers -l h_rt=${hrs}:00:00 ../submit_train.sh \
+     ${date}_icml_carbon${c}/train/g${num_gen} $HOME/ts4uc/data/envs/carbon/${num_gen}gen_carbon${c}.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch ; 
+ done ;
+
+
+# 20 gen
+num_gen=20
+workers=8
+epochs=200000
+hrs=18
+entropy_coef=0.001
+clip_ratio=0.1
+ac_lr=3e-05
+cr_lr=3e-04
+ac_arch="64,64"
+cr_arch="100,50,25"
+for c in 25 50;
+ do qsub -pe smp $workers -l h_rt=${hrs}:00:00 ../submit_train.sh \
+     ${date}_icml_carbon${c}/train/g${num_gen} $HOME/ts4uc/data/envs/carbon/${num_gen}gen_carbon${c}.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch ; 
+ done ; 
+
+# 30 gen
+num_gen=30
+workers=8
+epochs=300000
+hrs=24
+entropy_coef=0.0
+clip_ratio=0.1
+ac_lr=3e-05
+cr_lr=3e-04
+ac_arch="64,64"
+cr_arch="400,300"
+for c in 25 50; 
+do qsub -pe smp $workers -l h_rt=${hrs}:00:00 ../submit_train.sh \
+     ${date}_icml_carbon${c}/train/g${num_gen} $HOME/ts4uc/data/envs/carbon/${num_gen}gen_carbon${c}.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch ;
+ done ;
+
