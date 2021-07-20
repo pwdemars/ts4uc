6858fd9880244b465345e41d91364174ef5a454c
Patrick de Mars <pwdemars@gmail.com>
2021-07-20 19:54:06 +0100

    Renamed save_dir

--- local_scripts/302_milp_robust.sh
+++ local_scripts/302_milp_robust.sh
@@ -4,7 +4,7 @@
 date=$(date +"%y-%m-%d")
 
 for g in 10 20 30 ;
-do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_302/milp_g${g}_4sigma \
+do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_302/milp_g${g}_n_minus_one \
 						 --env_params_fn $HOME/ts4uc/data/envs/robustness/${g}gen.json \
 						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}gen/30min \
 						 --num_samples 1000 \
