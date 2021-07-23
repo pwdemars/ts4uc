2864d7dba91948336d660f9c02ba980d075b156b
Patrick de Mars <pwdemars@gmail.com>
2021-07-23 17:33:13 +0100

    Rename save_dir

--- local_scripts/302_milp_robust.sh
+++ local_scripts/302_milp_robust.sh
@@ -5,7 +5,7 @@ date=$(date +"%y-%m-%d")
 
 # iterate through 1..3 for n-1, n-2, n-3 and 10, 20, 30 gen problems
 for g in 1 2 3 ;
-do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_302/milp_g${g}0_n_minus_one \
+do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_302/milp_g${g}0_n_minus_x \
 						 --env_params_fn $HOME/ts4uc/data/envs/robustness/${g}0gen.json \
 						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}0gen/30min \
 						 --num_samples 1000 \
