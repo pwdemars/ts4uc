05cb1f2ea8bfa8fc51f0813342eae46b64b3e1b0
Patrick de Mars <pwdemars@gmail.com>
2021-07-23 07:56:07 +0100

    Fixed policy using different epoch

--- hpc_scripts/206-test_big.sh
+++ hpc_scripts/206-test_big.sh
@@ -15,7 +15,7 @@ for g in 100;
          qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_206/guided_${tree_search_func_name}/g${g}/g${g}_t${t}_p${rho}_${heuristic_method}_e${epoch} \
 														 $HOME/AISO_HPC/best_policies/g${g}/params.json \
 														 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
-														 $HOME/AISO_HPC/best_policies/g${g}/ac_final.pt \
+														 $HOME/AISO_HPC/best_policies/g${g}/ac_${epoch}.pt \
 														 ${t} \
 														 0.${rho} \
 														 ${tree_search_func_name} \
