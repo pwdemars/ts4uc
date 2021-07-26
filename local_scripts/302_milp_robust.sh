226190603b9ea02f4e8e85c54d0a70d56ad611be
Patrick de Mars <pwdemars@gmail.com>
2021-07-26 13:39:30 +0100

    Changed to just 30 gen

--- local_scripts/302_milp_robust.sh
+++ local_scripts/302_milp_robust.sh
@@ -3,12 +3,12 @@
 # set today's date to use as save directory
 date=$(date +"%y-%m-%d")
 
-# iterate through 1..3 for n-1, n-2, n-3 and 10, 20, 30 gen problems
-for g in 1 2 3 ;
-do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_302/milp_g${g}0_n_minus_x \
-						 --env_params_fn $HOME/ts4uc/data/envs/robustness/${g}0gen.json \
-						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}0gen/30min \
+g=30
+for x in 1 3 ;
+do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_302/milp_g${g}_n_minus_x \
+						 --env_params_fn $HOME/ts4uc/data/envs/robustness/${g}gen.json \
+						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}gen/30min \
 						 --num_samples 1000 \
 						 --reserve_sigma 4 \
-						 --n_minus_x ${g} ;
-done 					
+						 --n_minus_x ${x} ;
+done
