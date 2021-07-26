b8237424f528889531f129289822eaf9e8096d44
Patrick de Mars <pwdemars@gmail.com>
2021-07-26 11:48:30 +0100

    Added experiment for training policy with curtailment

---
+++ hpc_scripts/308-train_curtailment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# set today's date to use as save directory
+date=$(date +"%y-%m-%d")
+
+num_gen=30
+workers=8
+epochs=300000
+hrs=24
+entropy_coef=0.0
+clip_ratio=0.1
+ac_lr=3e-05
+cr_lr=3e-04
+ac_arch="64,64"
+cr_arch="400,300"
+qsub -pe smp $workers -l h_rt=${hrs}:00:00 ../submit_train.sh \
+     ${date}_icml_carbon${c}/train/g${num_gen} $HOME/ts4uc/data/envs/curtailment/${num_gen}gen.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch ;
