90df916161b44b00a833d56b406aae2d2c675b91
Patrick de Mars <pwdemars@gmail.com>
2021-08-06 10:20:53 +0100

    Added N-2

--- local_scripts/302_milp_robust.sh
+++ local_scripts/302_milp_robust.sh
@@ -4,7 +4,7 @@
 date=$(date +"%y-%m-%d")
 
 g=30
-for x in 1 3 ;
+for x in 1 2 3 ;
 do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_302/milp_g${g}_n_minus_${x} \
 						 --env_params_fn $HOME/ts4uc/data/envs/robustness/${g}gen.json \
 						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}gen/30min \
