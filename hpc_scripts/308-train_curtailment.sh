451c42b167c08677b1bcf31546a85db195e7194e
Patrick de Mars <pwdemars@gmail.com>
2021-08-11 14:31:46 +0100

    Updated training experiment for curtailment action of 50%

--- hpc_scripts/308-train_curtailment.sh
+++ hpc_scripts/308-train_curtailment.sh
@@ -14,7 +14,7 @@ cr_lr=3e-04
 ac_arch="64,64"
 cr_arch="400,300"
 for c in 0 25 50 ; 
-  do for r in 20 50 ; 
+  do for r in 50 ; 
      do qsub -pe smp $workers -l h_rt=${hrs}:00:00 ./submit_train.sh \
      ${date}_308/g${num_gen}_c${c}_${r} $HOME/ts4uc/data/envs/curtailment/${num_gen}gen_c${c}_${r}.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch ;
   done ; 
