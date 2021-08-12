e609f241a1f667c51cffbcfbb038ba9338c569fb
Patrick de Mars <pwdemars@gmail.com>
2021-08-12 09:12:40 +0100

    Typo

--- hpc_scripts/309-test_curtailment.sh
+++ hpc_scripts/309-test_curtailment.sh
@@ -11,9 +11,9 @@ for c in 0 25 50;
 	    do let secs=$t*48*3+500 && \
 	       time=$(date -d@$secs -u +%H:%M:%S) && \
 	       qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_309/g${g}_c${c}_r${r} \
-														 $HOME/AISO_HPC/best_polices/curtailment/g${g}_c${c}_${r}/params.json \
+														 $HOME/AISO_HPC/best_policies/curtailment/g${g}_c${c}_${r}/params.json \
 														 $HOME/ts4uc/data/envs/curtailment/${g}gen_c${c}_${r}.json \
-														 $HOME/AISO_HPC/best_polices/curtailment/g${g}_c${c}_${r}/ac_final.pt \
+														 $HOME/AISO_HPC/best_policies/curtailment/g${g}_c${c}_${r}/ac_final.pt \
 														 ${t} \
 														 0.${rho} \
 														 ${tree_search_func_name} \
