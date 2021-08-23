5e66109a4a638fee060c83dfffadd4012c802db5
Patrick de Mars <pwdemars@gmail.com>
2021-08-23 16:27:46 +0100

    Adding experiment for testing with custom reserve margin Note: requires some results from $HOME/thesis.../exp307

---
+++ local_scripts/310_milp_robust_custom_reserve.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set today's date to use as save directory
+date=$(date +"%y-%m-%d")
+
+g=30
+for x in 1 2 3 4;
+do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_310/milp_g${g}_n_minus_${x} \
+						 --env_params_fn $HOME/ts4uc/data/envs/robustness/${g}gen.json \
+						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}gen/30min \
+						 --num_samples 1000 \
+						 --custom_reserve_fn $HOME/thesis/output/chapter3/data/exp307_ida_star_reserves.csv ;
+
+done
