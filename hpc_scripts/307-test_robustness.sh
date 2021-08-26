7427086de89f95f499bd86dfee7cde988826b16d
Patrick de Mars <pwdemars@gmail.com>
2021-08-26 10:15:10 +0100

    Change save_dir

--- hpc_scripts/307-test_robustness.sh
+++ hpc_scripts/307-test_robustness.sh
@@ -12,7 +12,7 @@ heuristic_method="advanced_priority_list"
 for num_scenarios in 50 100 200 500 ;
 	do let secs=$t*48*3+500 && \
      time=$(date -d@$secs -u +%H:%M:%S) && \
-     qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_307/guided_${tree_search_func_name}/g${g}_p${rho}_s${num_scenarios} \
+     qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_307/guided_${tree_search_func_name}/g${g}_s${num_scenarios} \
 												 $HOME/AISO_HPC/best_policies/robustness/g${g}/params.json \
 												 $HOME/ts4uc/data/envs/robustness/${g}gen.json \
 												 $HOME/AISO_HPC/best_policies/robustness/g${g}/ac_final.pt \
