9ce6519cf75e9f75e76602fe5b12ee96ff325748
Patrick de Mars <pwdemars@gmail.com>
2021-08-26 10:36:56 +0100

    Adding rho back in, rename save_dir

--- hpc_scripts/307-test_robustness.sh
+++ hpc_scripts/307-test_robustness.sh
@@ -6,13 +6,14 @@ date=$(date +"%y-%m-%d")
 #
 g=30
 tree_search_func_name="ida_star"
+rho=05
 t=60
 seed=1
 heuristic_method="advanced_priority_list"
 for num_scenarios in 50 100 200 500 ;
 	do let secs=$t*48*3+500 && \
      time=$(date -d@$secs -u +%H:%M:%S) && \
-     qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_307/guided_${tree_search_func_name}/g${g}_s${num_scenarios} \
+     qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_307/g${g}_s${num_scenarios} \
 												 $HOME/AISO_HPC/best_policies/robustness/g${g}/params.json \
 												 $HOME/ts4uc/data/envs/robustness/${g}gen.json \
 												 $HOME/AISO_HPC/best_policies/robustness/g${g}/ac_final.pt \
