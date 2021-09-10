619c8c3c1e72e41f20854b9e6ffab7aff924041b
Patrick de Mars <pwdemars@gmail.com>
2021-09-10 07:48:12 +0100

    No reserve sigma when using N-x

--- local_scripts/302_milp_robust.sh
+++ local_scripts/302_milp_robust.sh
@@ -9,6 +9,6 @@ do python $HOME/pglib-uc/solve_and_test.py --save_dir ../results/${date}_302/mil
 						 --env_params_fn $HOME/ts4uc/data/envs/robustness/${g}gen.json \
 						 --test_data_dir $HOME/ts4uc/data/day_ahead/${g}gen/30min \
 						 --num_samples 1000 \
-						 --reserve_sigma 4 \
+						 --reserve_sigma 0 \
 						 --n_minus_x ${x} ;
 done
