129610174c459cd84304f13ee7ee231b20dca192
Patrick de Mars <pwdemars@gmail.com>
2021-12-02 10:35:53 +0000

    Added entropy target experiment

---
+++ hpc_scripts/208_entropy_target.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# set today's date to use as save directory
+date=$(date +"%y-%m-%d")
+
+# 100 gen
+num_gen=100
+workers=8
+hrs=48
+
+clip_ratio=0.1
+ac_lr=1e-05
+cr_lr=1e-04
+buffer_size=5000
+epochs=300000
+
+ac_arch="128,64"
+cr_arch="128,64"
+entropy_coef=0.
+entropy_target=0.
+for i in 1..20;
+     do qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh \
+          ${date}_207/g${num_gen}_v1 $HOME/ts4uc/data/day_ahead/${num_gen}gen/30min/env_params.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch $buffer_size $entropy_target ;
+done ;
+
+
+ac_arch="128,64"
+cr_arch="128,64"
+entropy_coef=1.
+entropy_target=0.1919
+for i in 1..20;
+     do qsub -pe smp $workers -l h_rt=${hrs}:00:00 submit_train.sh \
+          ${date}_207/g${num_gen}_v5 $HOME/ts4uc/data/day_ahead/${num_gen}gen/30min/env_params.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch $buffer_size $entropy_target ;
+done ;
