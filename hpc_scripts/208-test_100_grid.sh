77f17bb499154e709d0309c9797e9b2ccb326e42
Patrick de Mars <pwdemars@gmail.com>
2021-12-18 07:50:07 +0000

    Adjusted version names

--- hpc_scripts/208-test_100_grid.sh
+++ hpc_scripts/208-test_100_grid.sh
@@ -11,7 +11,7 @@ tree_search_func_name="ida_star"
 g=100
 heuristic_method="advanced_priority_list"
 t=60
-for v in 1 2 3 4 5 6 7;
+for v in 0 1 2 3 4 5 6 ;
     do let secs=$t*48*3+500 && \
        time=$(date -d@$secs -u +%H:%M:%S) && \
        qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_208/guided_${tree_search_func_name}/g${g}/g${g}_t${t}_p${rho}_${heuristic_method}_e${epoch} \
