b78dcdc74509a1e92726974d09eef5f88b54f80f
Patrick de Mars <pwdemars@gmail.com>
2021-12-19 10:01:02 +0000

    Added version to save_dir

--- hpc_scripts/208-test_100_grid.sh
+++ hpc_scripts/208-test_100_grid.sh
@@ -14,7 +14,7 @@ t=60
 for v in 0 1 2 3 4 5 6 ;
     do let secs=$t*48*3+500 && \
        time=$(date -d@$secs -u +%H:%M:%S) && \
-       qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_208/guided_${tree_search_func_name}/g${g}/g${g}_t${t}_p${rho}_${heuristic_method}_e${epoch} \
+       qsub -l h_rt=$time submit_anytime_tree_search.sh $HOME/Scratch/results/${date}_208/guided_${tree_search_func_name}/g${g}_v${v}/g${g}_t${t}_p${rho}_${heuristic_method}_e${epoch} \
 													 $HOME/AISO_HPC/best_policies/exp207/g100_v${v}/params.json \
 													 $HOME/ts4uc/data/day_ahead/${g}gen/30min/env_params.json \
 													 $HOME/AISO_HPC/best_policies/exp207/g100_v${v}/ac_${epoch}.pt \
