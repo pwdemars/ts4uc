81a8b9ce084a19401df003b20a8466df3ada90b5
Patrick de Mars <pwdemars@gmail.com>
2022-08-10 17:00:19 +0100

    Increased buffer size

--- hpc_scripts/convex_opt_paper/train.sh
+++ hpc_scripts/convex_opt_paper/train.sh
@@ -12,8 +12,9 @@ ac_lr=3e-05
 cr_lr=3e-04
 ac_arch="64,64"
 cr_arch="400,300"
+buffer_size=5000
 
 for num_gen in {40,50} ; 
   do qsub -pe smp $workers -l h_rt=${hrs}:00:00 ../submit_train.sh \
-     ${date}_convex_opt_comp/g${num_gen} $HOME/AISO_HPC/AISO/rl4uc/rl4uc/data/envs/${num_gen}gen.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch ;
+     ${date}_convex_opt_comp/g${num_gen} $HOME/AISO_HPC/AISO/rl4uc/rl4uc/data/envs/${num_gen}gen.json $workers $epochs $entropy_coef $clip_ratio $ac_lr $cr_lr $ac_arch $cr_arch $buffer_size ;
 done
